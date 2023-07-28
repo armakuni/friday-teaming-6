e517c927708fe80d0946ac9eac1ec576326b6c98
Ben Nagy <nagy.benb@gmail.com>
2023-07-28 10:45:27 +0100

    wip

--- character.py
+++ character.py
@@ -5,16 +5,15 @@ DEAD = 2
 class Character:
     def __init__(self) -> None:
         self.hp = 1000
-        self.state = ALIVE
 
     def get_hp(self):
         return self.hp
 
     def get_state(self):
-        return self.state
+        if self.hp <= 0:
+            return DEAD
+
+        return ALIVE
 
     def take_damage(self, damage_amount):
         self.hp -= damage_amount
-
-        if self.hp <= 0:
-            self.state = DEAD
