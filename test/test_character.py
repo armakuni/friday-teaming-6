10b5272d4f61cb8ed16b1801c1fdca71088a4932
Ben Nagy <nagy.benb@gmail.com>
2023-07-28 09:37:51 +0100

    use pytest & fix pipenv

--- test/test_character.py
+++ test/test_character.py
@@ -1,5 +1,5 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+
+def test_something():
+  assert True
