d4eb3a1b5344e49a7ee9d5ddd7e57fa3060c0d77
Ben Nagy <nagy.benb@gmail.com>
2023-07-28 10:25:20 +0100

    wip

--- test/test_character.py
+++ test/test_character.py
@@ -1,4 +1,4 @@
-from character import Character
+from character import *
 
 
 def test_something():
@@ -9,3 +9,9 @@ def test_character_starts_with_1000_hp():
     character = Character()
 
     assert character.get_hp() == 1000
+
+
+def test_character_starts_with_alive_state():
+    character = Character()
+
+    assert character.get_state() == ALIVE
