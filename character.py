f22be825c09174e4f01c66d1aa7d7f3ce234dc64
Ben Nagy <nagy.benb@gmail.com>
2023-07-28 10:37:58 +0100

    add missing files

---
+++ character.py
@@ -0,0 +1,16 @@
+ALIVE = 1
+DEAD = 2
+
+
+class Character:
+    def __init__(self) -> None:
+        self.hp = 1000
+
+    def get_hp(self):
+        return self.hp
+
+    def get_state(self):
+        return ALIVE
+
+    def take_damage(self, damage_amount):
+        return
